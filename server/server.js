f88a285e997ae972d6c9a3e8ca8f41798d79d77c
ParthMishra <124814034+ParthMishra20@users.noreply.github.com>
2024-12-08 03:16:39 +0530

    commit

--- server/server.js
+++ server/server.js
@@ -29,6 +29,6 @@ app.get('/', (req, res) => {
 
 // Start server
 const PORT = process.env.PORT || 5001;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
